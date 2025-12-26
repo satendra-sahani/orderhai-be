fa9fa41c28af30a9bcc5d7e52ebac622b4bad80c
Satendra Sahani <satendrasahani270@gmail.com>
2025-12-27 02:08:28 +0530

    update config for vercel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 // src/server.js
-import app from "./app.js"
+// import app from "./app.js"
 import { connectDB } from "./config/db.js"
 import { PORT } from "./config/env.js"
 
@@ -7,9 +7,9 @@ const start = async () => {
   try {
     await connectDB()
     const port = PORT || 5000
-    app.listen(port, () => {
-      console.log(`API server running on http://localhost:${port}`)
-    })
+    // app.listen(port, () => {
+    //   console.log(`API server running on http://localhost:${port}`)
+    // })
   } catch (err) {
     console.error("Failed to start server:", err.message)
     process.exit(1)
