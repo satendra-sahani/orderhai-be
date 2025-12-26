10bdbea09fafabe55e14d06ee838e8f1320cc723
Satendra Sahani <satendrasahani270@gmail.com>
2025-12-27 02:45:04 +0530

    fix issue

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,4 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-export const { PORT, MONGO_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env
+export const { PORT, MONGO_URI = "mongodb+srv://aiwats748:vSGV3vcPmrb4rOZ1@cluster0.swjkv.mongodb.net/aiwats", JWT_SECRET, JWT_EXPIRES_IN } = process.env
